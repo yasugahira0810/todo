a07eef7ce9dc8109286d890e7317eb4722bd6423
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-01-14 16:22:43 +0000

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 20160113170924) do
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
-    t.integer  "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "status",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "users", force: :cascade do |t|
