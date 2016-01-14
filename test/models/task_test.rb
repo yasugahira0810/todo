74697e8cdd94b4ac1b6aed07208ac8ce928c232a
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-01-14 16:03:46 +0000

    Add presence test

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+      @task = Task.new(name: "振込", content: "水道代を振り込む", status: 0)
+  end
+
+  test "name should be present" do
+    @task.name = "      "
+    assert_not @task.valid?
+  end
+
+  test "content should be present" do
+    @task.content = "     "
+    assert_not @task.valid?
+  end
+
+  test "status should be present" do
+    @task.status = "      "
+    assert_not @task.valid?
+  end
+
 end
