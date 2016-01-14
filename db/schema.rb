bceb1373a49c6507cbc4b37dd100c91c0b2deb77
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-01-14 15:34:04 +0000

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921141641) do
+ActiveRecord::Schema.define(version: 20160113170924) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.integer  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
