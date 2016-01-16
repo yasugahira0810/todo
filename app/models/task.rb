0093b8ec5f6bc687d3882a65b8776d0bd7e6d3d4
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-01-16 15:43:42 +0000

    Add status definition

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,15 @@ class Task < ActiveRecord::Base
     validates :name,  presence: true
     validates :content,  presence: true
     validates :status,  presence: true
+
+    NOT_YET = 0
+    DONE = 1
+    PENDING = 2
+    
+    STATUS = {
+            'NOT_YET' => NOT_YET,
+            'DONE' => DONE,
+            'PENDING' => PENDING
+    }.freeze
+
 end
