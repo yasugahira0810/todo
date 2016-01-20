7a23090b93fb8cf251582f6e9bf67a7c513a95ac
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-01-20 15:40:27 +0000

    implement new action for task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,30 @@ class TasksController < ApplicationController
     def index
             @tasks = Task.all
     end
+
+    def new
+            @task = Task.new
+    end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      # success
+      flash[:info] = "Created new task!"
+      redirect_to tasks_path
+      #log_in @user
+      #flash[:success] = "Welcome to the Sample App!"
+      #redirect_to @user
+    else
+      # failure
+      render 'new'
+    end
+  end
+
+  private
+  
+    def task_params
+      params.require(:task).permit(:name, :content, :status)
+    end
+
 end
