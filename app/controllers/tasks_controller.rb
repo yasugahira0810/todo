11283028a005dcfc2e0a00c207f72657eaabced8
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-01-23 09:10:35 +0000

    implement task query parameters

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,18 @@
 class TasksController < ApplicationController
     def index
-            @tasks = Task.all
+      @tasks = Task.order('created_at DESC')
+      if @status = params[:status]
+        case @status
+        when 'not_yet'
+        @tasks = @tasks.status_is_not_yet
+        when 'pending'
+        @tasks = @tasks.status_is_pending
+        when 'done'
+        @tasks = @tasks.status_is_done
+        else
+          @status = nil
+          end
+      end
     end
 
     def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,12 @@ class Task < ActiveRecord::Base
     validates :content,  presence: true
     validates :status,  presence: true
 
+    scope :status_is, ->(status) { where(status: status) }
+
+    scope :status_is_not_yet, -> { status_is(NOT_YET) }
+    scope :status_is_pending, -> { status_is(PENDING) }
+    scope :status_is_done, -> { status_is(DONE) }
+
     NOT_YET = 0
     DONE = 1
     PENDING = 2
